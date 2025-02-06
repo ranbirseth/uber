9212842b88fcbdd799dd2657da2e3e9d3cf3250a
ranbirseth <153383602+ranbirseth@users.noreply.github.com>
2025-02-06 12:53:42 +0530

    first commit

---
+++ Backend/Server.js
@@ -0,0 +1,8 @@
+const http = require('http')
+const app = require('./App')
+const port = process.env.PORT || 3000 ; 
+
+const server = http.createServer(app)
+server.listen(port , ()=>{
+    console.log(`server is runing on port ${port}`);
+})
